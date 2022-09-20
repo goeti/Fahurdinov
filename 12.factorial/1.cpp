a68774c155b44b2555671cda728fb675564f2f8c
ильяс фахурдинов <mv1451003@gmail.com>
2022-09-20 16:44:43 +0300

    da

---
+++ 12.factorial/1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <limits> 
+#include <iomanip>
+
+using namespace std;
+long double just(int w){
+	if (w==1) return 1;
+	return w*just(w-1);
+}
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int w;
+	while(true){
+		cout<<"Введите натуральное число: ";
+		cin>>w;
+	    if (cin.get() == (int) '\n'){
+			if (w>0 && w<100000){
+				cout<<just(w)<<"\n";
+				break;}
+			else cout << "Это не натуральное число\n";
+		}
+		    
+		else{
+			cout << "Это не число\n";
+			cin.clear();
+			cin.ignore(32767, '\n');}
+	}
+	return 0;
+}
