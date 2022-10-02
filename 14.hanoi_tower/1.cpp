fc22b6bea997fe350a32c25cff2e11c6cadd2e56
ильяс фахурдинов <mv1451003@gmail.com>
2022-10-02 22:45:02 +0300

    da

---
+++ 14.hanoi_tower/1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <limits> 
+#include <iomanip>
+
+using namespace std;
+
+void hanoi(int n, int f, int t, int p){
+	if (n != 0){
+		hanoi(n-1,f, p, t);
+		cout<<f<<" -> "<<t<<endl;
+		hanoi(n-1,p, t, f);
+	}
+}
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int n;
+	cout<<"Введите N:";
+	cin>>n;
+	hanoi(n,1,3,2);
+	return 0;
+}
